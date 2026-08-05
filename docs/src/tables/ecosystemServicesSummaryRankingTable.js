eb2b3eac18e84fed985600bb995d124b4867a4d9
Ginny Douglas <g.k.douglas@hotmail.co.uk>
2026-08-05 22:23:34 +0100

    Add vuln-compact CSV (252k companies, 41MB); fix ranking table and business vuln to load all companies

--- docs/src/tables/ecosystemServicesSummaryRankingTable.js
+++ docs/src/tables/ecosystemServicesSummaryRankingTable.js
@@ -842,7 +842,7 @@ export function initEcosystemServicesSummaryRankingTable() {
     queueRender();
   });
 
-  fetchDashboardDataText("dashboard_company_compact.csv", "dashboard company compact")
+  fetchDashboardDataText("dashboard_scatter_compact.csv", "dashboard scatter compact")
     .then((compactCsv) => {
       records = buildRecords(parseTable(compactCsv));
       lastComputedKey = "";
